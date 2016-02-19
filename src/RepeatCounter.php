a90667342bd0c169779f49e1d06013b50bca829c
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-19 11:39:53 -0800

    Add views pages

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,12 +1,21 @@
 <?php
     class RepeatCounter
     {
-
         function countRepeats($word_input, $phrase_input)
         {
+          $phrase_input = preg_replace('/[^a-z0-9]+/i', ' ', $phrase_input);
+          $phrase_input = strtolower($phrase_input);
+          $phrase_array = explode(" ", $phrase_input);
           $counter = 0;
 
-          $array = explode(" ", $input_phrase);
+          foreach($phrase_array as $word)
+          {
+              if($word_input == $word)
+              {
+                $counter = $counter + 1;
+              }
+            return $counter;
+          }
         }
     }
  ?>
--- tests/RepeatCounterTest.php
+++ tests/RepeatCounterTest.php
@@ -1,53 +1,54 @@
 <?php
-require_once "src/RepeatCounter.php";
+  require_once "src/RepeatCounter.php";
 
-class RepeatCounterTest extends PHPUnit_Framework_TestCase{
+  class RepeatCounterTest extends PHPUnit_Framework_TestCase
+  {
 
-  function test_RepeatCounter_notPresent()
-  {
-    //Arrange
-    $test_RepeatCounter = new RepeatCounter;
-    $word_input = "henry";
-    $phrase_input = "I love coffee";
-    //Act
-    $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
-    //Assert
-    $this->assertEquals("0", $result);
-  }
-  function test_RepeatCounter_oneMatch()
-  {
-    //Arrange
-    $test_RepeatCounter = new RepeatCounter;
-    $word_input = "coffee";
-    $phrase_input = "coffee";
-    //Act
-    $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
-    //Assert
-    $this->assertEquals("1", $result);
-  }
-  function test_RepeatCounter_amongOtherWords()
-  {
-    //Arrange
-    $test_RepeatCounter = new RepeatCounter;
-    $word_input = "coffee";
-    $phrase_input = "I love coffee";
-    //Act
-    $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
-    //Assert
-    $this->assertEquals("1", $result);
-  }
-  function test_RepeatCounter_multiple()
-  {
-    //Arrange
-    $test_RepeatCounter = new RepeatCounter;
-    $word_input = "captain";
-    $phrase_input = "Oh captain my captain";
-    //Act
-    $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
-    //Assert
-    $this->assertEquals("2", $result);
-  }
+      function test_RepeatCounter_notPresent()
+      {
+        //Arrange
+        $test_RepeatCounter = new RepeatCounter;
+        $word_input = "henry";
+        $phrase_input = "I love coffee";
+        //Act
+        $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
+        //Assert
+        $this->assertEquals(0, $result);
+      }
+      function test_RepeatCounter_oneMatch()
+      {
+        //Arrange
+        $test_RepeatCounter = new RepeatCounter();
+        $word_input = "coffee";
+        $phrase_input = "coffee";
+        //Act
+        $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
+        //Assert
+        $this->assertEquals(1, $result);
+      }
+      function test_RepeatCounter_amongOtherWords()
+      {
+        //Arrange
+        $test_RepeatCounter = new RepeatCounter;
+        $word_input = "coffee";
+        $phrase_input = "I love coffee";
+        //Act
+        $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
+        //Assert
+        $this->assertEquals(1, $result);
+      }
+      function test_RepeatCounter_multiple()
+      {
+        //Arrange
+        $test_RepeatCounter = new RepeatCounter;
+        $word_input = "captain";
+        $phrase_input = "Oh captain my captain";
+        //Act
+        $result = $test_RepeatCounter->countRepeats($word_input, $phrase_input);
+        //Assert
+        $this->assertEquals(2, $result);
+      }
 
-}
+    }
 
  ?>

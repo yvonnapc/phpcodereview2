73b0406126020669d1488acfe10b389cf16b56c6
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-19 13:42:49 -0800

    Add getters and setters

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,22 +1,51 @@
 <?php
     class RepeatCounter
     {
-        function countRepeats($word_input, $phrase_input)
-        {
-          $phrase_input = preg_replace('/[^a-z0-9]+/i', ' ', $phrase_input);
-          $phrase_input = strtolower($phrase_input);
-          $phrase_array = explode(" ", $phrase_input);
-          $counter = 0;
+      private $word_input;
+      private $phrase_input;
 
-          foreach($phrase_array as $word)
-            {
-                if($phrase_input == $word)
-                {
-                  $counter = $counter + 1;
-                }
+      function __construct($inputtedWord, $inputtedPhrase)
+      {
+        $this->word = $inputtedWord;
+        $this->phrase = $inputtedPhrase;
+      }
+      function setWord($new_word_input)
+      {
+        $this->word = $new_word_input;
+      }
+      function getWord()
+      {
+        return $this->word;
+      }
+      function setPhrase($new_phrase_input)
+      {
+        $this->phrase = $new_phrase_input;
+      }
+      function getPhrase()
+      {
+        return $this->phrase;
+      }
+      function countRepeats($word_input, $phrase_input)
+      {
+        return preg_match_all("/\b($this->word)\b/i", $this->phrase, $matched);
+      }
 
-            }return $counter;
+          // $stringInput = preg_match('/[^a-z0-9]+/gi', ' ', $phrase_input);
+          // $stringInput = strtolower($word_input);
+          //
+          // $phrase_array = explode(" ", $phrase_input);
+          //
+          // $counter = 0;
+          //
+          // foreach($phrase_array as $word)
+          //   {
+          //       if($phrase_input == $word)
+          //       {
+          //         $counter = $counter + 1;
+          //       }
+          //
+          //   }return $counter;
 
-        }
+
     }
  ?>

30dc7e77411d3a9832df03ec337c39f7e1bd548d
Yvonna Contreras <yvonnap.contreras@gmail.com>
2016-02-19 16:06:54 -0800

    Add CSS

--- src/RepeatCounter.php
+++ src/RepeatCounter.php
@@ -1,51 +1,68 @@
 <?php
     class RepeatCounter
+
     {
-      private $word_input;
-      private $phrase_input;
+        function CountRepeats($sentence_input, $word_input)
+        {
+            $sentence_input = preg_replace('/[^a-z0-9]+/i', ' ', $sentence_input);
+            $sentence_input = strtolower($sentence_input);
+            $sentence_array = explode(" ",$sentence_input);
+            $counter = 0;
 
-      function __construct($inputtedWord, $inputtedPhrase)
-      {
-        $this->word = $inputtedWord;
-        $this->phrase = $inputtedPhrase;
-      }
-      function setWord($new_word_input)
-      {
-        $this->word = $new_word_input;
-      }
-      function getWord()
-      {
-        return $this->word;
-      }
-      function setPhrase($new_phrase_input)
-      {
-        $this->phrase = $new_phrase_input;
-      }
-      function getPhrase()
-      {
-        return $this->phrase;
-      }
-      function countRepeats($word_input, $phrase_input)
-      {
-        return preg_match_all("/\b($this->word)\b/i", $this->phrase, $matched);
-      }
+            foreach($sentence_array as $word)
+            {
+                if($word_input == $word)
+                {
+                    $counter++;
+                }
+            }
+            return $counter;
+        }
+    }
+    // {
+    //   private $word;
+    //   private $phrase;
+    //
+    //   function __construct($inputtedWord, $inputtedPhrase)
+    //   {
+    //     $this->word = $inputtedWord;
+    //     $this->phrase = $inputtedPhrase;
+    //   }
+    //   function setWord($newWord)
+    //   {
+    //     $this->word = $newWord;
+    //   }
+    //   function getWord()
+    //   {
+    //     return $this->word;
+    //   }
+    //   function setPhrase($newPhrase)
+    //   {
+    //     $this->phrase = $newPhrase;
+    //   }
+    //   function getPhrase()
+    //   {
+    //     return $this->phrase;
+    //   }
+      // function countRepeats($word_input, $phrase_input)
+      // {
+      //   $counter = 0;
+      //
+      //   $stringPhrase = preg_match_all('/[^a-z0-9]+/gi', ' ', $phrase_input);
+        // return preg_match_all("/\b($this->word)\b/i", $this->phrase, $matched);
+      // }  return $counter;
 
-          // $stringInput = preg_match('/[^a-z0-9]+/gi', ' ', $phrase_input);
+          // $stringInput =
           // $stringInput = strtolower($word_input);
           //
           // $phrase_array = explode(" ", $phrase_input);
           //
-          // $counter = 0;
+          //
           //
           // foreach($phrase_array as $word)
           //   {
           //       if($phrase_input == $word)
           //       {
           //         $counter = $counter + 1;
-          //       }
-          //
-          //   }return $counter;
-
-
-    }
- ?>
+          //       } return $counter
+?>
